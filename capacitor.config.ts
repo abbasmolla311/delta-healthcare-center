64e3e566f692eff98767cdbefcacd92a1d578c8e
zyanmaahin133 <zyanmaahin133@gmail.com>
2026-02-09 18:13:19 +0530

    feat: Add Android platform and fix configuration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,11 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.92d4f550976f4985b6045233adb75cc9',
+  appId: 'com.taj.store',
   appName: 'TAJ Store',
   webDir: 'dist',
   server: {
-    url: 'https://92d4f550-976f-4985-b604-5233adb75cc9.lovableproject.com?forceHideBadge=true',
+    url: 'https://tajmedicalstore.netlify.app/',
     cleartext: true
   }
 };
